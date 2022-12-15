fb16eba4ca37e4818e5b331ad612c42aa35e46d1
Tuan Truong <truong.anh.tuan@sun-asterisk.com>
2022-12-15 14:30:38 +0700

    Add main screen

---
+++ ModuleDemo/Main/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  ModuleDemo
+//
+//  Created by Tuan Truong on 15/12/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
