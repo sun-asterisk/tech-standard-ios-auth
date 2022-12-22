b988aa0ef18528ca926a07ebd0b2c3cde52920c6
Tuan Truong <truong.anh.tuan@sun-asterisk.com>
2022-12-22 15:43:21 +0700

    Add example

---
+++ AuthExample/AuthExample/AuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthExampleApp.swift
+//  AuthExample
+//
+//  Created by Tuan Truong on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
